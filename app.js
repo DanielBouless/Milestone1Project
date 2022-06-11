ef000f15efde0389f7c0c4c94f89c8a25009615c
Daniel Bouless <dbouless76@gmail.com>
2022-06-11 09:51:18 -0400

    made it move

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const scoreDiv = document.getElementById('score');
 const multiplerDiv = document.getElementById('multipler');
 const timerDiv = document.getElementById('timer');
 const gameboardDiv = document.getElementById('gameboard');
+const findMeDiv = document.getElementById('find-me');
 const startBtn = document.getElementById('start-btn');
+const playState = document.getElementById('play-state');
+
 
 let clicks=0;
 let initScore = 100;
@@ -16,32 +19,45 @@ let timer = 120;
 
 //Get number of gameboard clicks
 gameboardDiv.addEventListener('click',()=>{
-    clicks += 1
-    let clicksRemaining = maxClicks - clicks
-    counterDiv.textContent = clicksRemaining
+    clicks += 1;
+    let clicksRemaining = maxClicks - clicks;
+    counterDiv.textContent = clicksRemaining;
 
     //Update Multiplier after every click
-    changeMulti(clicks, clicksForMulti)
+    changeMulti(clicks, clicksForMulti);
 
     //display score after every click
-    displayScore(clicks)
+    displayScore(clicks);
 })
 
-
-//start timer when you hit start button
+//Create events when hit start
+        //start timer when you hit start button, change "Ready?" to "SEEK!", change display to black and text to white, randomize black box location
 
 startBtn.addEventListener('click',()=>{
-    refreshTimer()
-    })
-
-async function refreshTimer(){
+    //start timer
     setInterval(()=>{
     timer--;
     timerDiv.textContent = timer;
-    },1000)
+    if(timer <=0){
+        timerDiv.textContent = 0;
     }
+    },1000);
 
+    //change words
+    playState.textContent = 'SEEK!';
 
+    //change colors
+    document.body.style.backgroundColor = 'black';
+    document.getElementById('container-1').style.color = 'white';
+    gameboardDiv.style.backgroundColor = 'blue';
+
+    //change location of square
+    let topDist = Math.floor(Math.random()* gameboardDiv.offsetHeight);
+    let leftDist = Math.floor(Math.random()* gameboardDiv.offsetWidth);
+    findMeDiv.style.cssText = 'bottom:'+ topDist + 'px; left:'+ leftDist + 'px;';
+    })
+
+    
 
 //Display score Function
         //Scoring formula = [InitalScore - (#ofClicks*CurrentScore)]*X, where X = Multiplier + (Bet*2) and Bet <= ScoreBeforePlaying. X gets shown at the end of the game for final score
