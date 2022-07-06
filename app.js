f8bf0b273669fcd5ac171d5868e790c5c2093063
Daniel Bouless <dbouless76@gmail.com>
2022-07-06 17:39:50 -0400

    moved V2 to main folder and removed old version of game

--- app.js
+++ app.js
@@ -1,223 +1,198 @@
-const counterDiv = document.getElementById('counter');
-const scoreDiv = document.getElementById('score');
-const multiplerDiv = document.getElementById('multiplier');
-const timerDiv = document.getElementById('timer');
-const gameboardDiv = document.getElementById('gameboard');
-const findMeDiv = document.getElementById('find-me');
-const startBtn = document.getElementById('start-btn');
-const playState = document.getElementById('play-state');
+
+/*
+
+
+MECHANICS OF THE GAME:
+
+
+A) EACH TIME THE SUBMIT BUTTON IS HIT:
+    1) Hide SUBMIT BUTTON / show START BUTTON
+    2) Initiate FUNCTION to set BET amount
+
+
+B) EACH TIME THE START BUTTON IS HIT:
+    1) Change color palette
+    2) Create an EVENT LISTENER on the GAMEBOARD to track CLICKS
+        a) Stop game and mark as loss if click limit reached
+    3) Create an EVENT LISTENER on FINDME
+        a) Stop game and mark as win if selected
+    4) Initiate countdown TIMER FUNCTION
+        a) Stop game if timer reaches 0
+        b) Stop timer if FINDME selected
+    5) Initiate FUNCTION to update SCOREBOARD
+    6) Hide START BUTTON / show RESET BUTTON
+
+
+C) EACH TIME THE RESET BUTTON IS HIT:
+    1) Stop TIMER & reset
+    2) Reset scoreboard
+    3) Remove FINDME EVENT LISTENER
+    4) Remove GAMMEBOARD EVENT LISTENER
+    5) Hide RESET Button / show SUBMIT BUTTON
+
+
+NOTES:
+    1. Show Reset button always and only alternate between hiding the submit and start button
+    2. Make the game board one size as well as the game settings and especially the scoreboard
+    3. Winning removes the timer. Starting the game creates a new timer
+    4. If no bet value is entered, default to 0
+    6. Create PLAYSTATE / WinnerStatus and what happens if win or lose
+        a. Decide ==> submit button
+        b. Play ==> start button
+        c. WinnerCircle ==> Add bet to score, Reset Everything
+        d. LoserPit ==> Subtract bet from score, Reset Everything
+*/
 const submitBtn = document.getElementById('submit-btn');
-const insultDiv = document.getElementById('distractions');
+const startBtn = document.getElementById('start-btn');
+const resetBtn = document.getElementById('reset-btn');
+const betInput = document.getElementById('bet');
+const currentBet = document.getElementById('current-bet');
+const gameboard = document.getElementById('gameboard');
+const findMe = document.getElementById('find-me');
+const counter = document.getElementById('counter');
+const container1 = document.getElementById('container-1');
+const gameOpt = document.getElementById('game-options');
+const playerInfo = document.getElementById('player-info');
+const exampleDot = document.getElementById('example-dot');
+const presetDiff = document.getElementById('preset-difficulty');
+const scoreboard = document.getElementById('score');
+const timerDiv = document.getElementById('timer');
 
 
-let clicks=0;
-let multiplier = 1;
-let initScore = 420;
-let score;
-let bet = 0;
-let maxClicks=69;
+let maxClicks = 69;
+let clicks = 0;
+let score = 0;
+let diffState;
+let playState= 'decide';
 let startTimer;
 let timer = 420;
-let ptsPerClick;
-let diffState;
-let moneyBag = 0;
-const insults = ['Nope', 'HA! Nope!','Not that one', 'Nah mah dude','FIND ME!!!!','Yeah, you win...']
 
-//Setup game
-//create event listener on submit-btn to update playerInfo
 
-submitBtn.addEventListener('click', ()=>{
-   
-    //timer, also clearInterval if timer less than 420
-    timerDiv.textContent =  timer;
-     if(timer<420){
+presetDiff.addEventListener('change', setDiff); 
+
+/////////////////// SUBMIT BUTTON \\\\\\\\\\\\\\\\\\\\\\\
+
+submitBtn.addEventListener('click',()=>{
+            currentBet.textContent = betInput.value;
+            changeDisplay('start');
+            presetDiff.removeEventListener('change', setDiff);      
+});
+
+//\\\\\\\\\\\\\\\\\ SUBMIT BUTTON //////////////////////\\
+
+/////////////////// START BUTTON  \\\\\\\\\\\\\\\\\\\\\\\
+
+startBtn.addEventListener('click',()=>{
+    changeDisplay('playing');
+    betInput.value = 0;
+    startTimer = setInterval(myTimer,1000);
+    gameboard.addEventListener('click', look4Clicks);
+    findMe.style.cssText += moveBox();
+
+
+    findMe.addEventListener('click',()=>{
+        changeDisplay('winner');
+        gameboard.removeEventListener('click',look4Clicks);
+        presetDiff.addEventListener('change', setDiff);
         clearInterval(startTimer);
-        timer = 420;
-        timerDiv.textContent =  timer;
-    }
+        displayScore();
+    });
 
-    //change multiplier
-    multiplier = 1;
-    multiplerDiv.textContent = multiplier;
-    
 
-    //Store Bet amount
-    bet = document.getElementById('bet').value;
-    document.getElementById('bet').value = '';
+});
 
-    //Set Difficulty
 
-    switch (document.getElementById('preset-difficulty').value){
+/////////////////// START BUTTON  \\\\\\\\\\\\\\\\\\\\\\\
+
+/////////////////// RESET BUTTON  \\\\\\\\\\\\\\\\\\\\\\\
+
+resetBtn.addEventListener('click', ()=>{
+    changeDisplay('decide');
+    clearInterval(startTimer);
+    timer = 420;
+    score = 0;
+
+});
+
+
+/////////////////// RESET BUTTON  \\\\\\\\\\\\\\\\\\\\\\\
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+function changeDisplay(playState){
+        switch(playState){
+    case 'decide':
+        startBtn.style.display = 'none';
+        submitBtn.style.display = 'inline-block';
+        break;
+    case 'start':
+        startBtn.style.display = 'inline-block';
+        submitBtn.style.display = 'none';
+        break;
+    case 'playing':
+        startBtn.style.display = 'none';
+        submitBtn.style.display = 'none';
+        document.body.style.backgroundColor = 'black';
+        findMe.style.backgroundColor = 'black';
+        exampleDot.style.backgroundColor = 'red';
+        container1.style.color = 'red';
+        container1.style.backgroundColor = 'black';
+        gameboard.style.backgroundColor = 'black';
+        gameOpt.style.backgroundColor = 'black';
+        gameOpt.style.color = 'red';
+        gameOpt.style.border = '5px solid red'
+        playerInfo.style.backgroundColor = 'black';
+        playerInfo.style.color = 'red';
+        playerInfo.style.border = '5px solid red';        
+        break;
+    case 'winner':
+        startBtn.style.display = 'none';
+        submitBtn.style.display = 'inline-block';
+        findMe.style.backgroundColor = 'red';
+        break;
+    case 'loser':
+        startBtn.style.display = 'none';
+        submitBtn.style.display = 'inline-block';
+        break;
+    default:
+        
+}
+}
+
+
+function setDiff(){
+    switch(presetDiff.value){
         case 'Easy':
-            diffState = setDifficulty('100px');
+            setBoxSize('100px;');
             break;
         case 'Normal':
-            diffState = setDifficulty('10px');
+            setBoxSize('10px;');
             break;
         case 'Hard':
-            diffState = setDifficulty('5px');
+            setBoxSize('5px;');
             break;
-        default:
+            default:
     }
-
-    //Hide submit-btn and show Start-btn
-    submitBtn.style.visibility = 'hidden';
-    startBtn.style.display = 'inline';
-
-
-
-   
-})
-
-// //Get number of gameboard clicks
-// gameboardDiv.addEventListener('click',()=>{
-//     clicks += 1;
-//     let clicksRemaining = maxClicks - clicks;
-//     counterDiv.textContent = clicksRemaining;
-//     if(clicksRemaining<=0){
-//         clicks = 0;
-//         clicksRemaining = 0;
-//         bet = 0;
-//         score = score;
-//         counterDiv.textContent = clicksRemaining;
-//         insultDiv.textContent = 'Loser :)'
-//         timer = timer;
-//     }
-
-
-//     //Update Multiplier after every click
-//     changeMulti(clicks);
-
-//     //display score after every click
-//     displayScore(clicks);
-    
-//     //display different insult for every click
-//     displayInsult();
-// })
-
-
-
-    //start timer
-    startTimer = setInterval(myTimer,1000)
-   
-    //change words
-    playState.textContent = 'SEEK!';
-
-    //change colors
-    document.body.style.backgroundColor = 'black';
-    document.getElementById('container-1').style.color = 'red';
-    document.getElementById('container-1').style.backgroundColor = 'black';
-    gameboardDiv.style.backgroundColor = 'black';
-    document.getElementById('game-options').style.backgroundColor = 'black';
-    document.getElementById('game-options').style.color = 'red';
-    document.getElementById('game-options').style.border = '5px solid red'
-    document.getElementById('player-info').style.backgroundColor = 'black';
-    document.getElementById('player-info').style.color = 'red';
-    document.getElementById('player-info').style.border = '5px solid red';
-    
-
-    //change location of square
-    let topDist = Math.floor(Math.random()* (gameboardDiv.offsetHeight - findMeDiv.offsetHeight));
-    let leftDist = Math.floor(Math.random()* (gameboardDiv.offsetWidth- findMeDiv.offsetWidth)); 
-    findMeDiv.style.cssText = 'bottom:'+ topDist + 'px; left:'+ leftDist + 'px;'+ diffState;
-    
-    //hide Start-btn and show submit-btn
-    startBtn.style.display = 'none';
-    submitBtn.style.display = 'inline';
-
-
-
-        //Get number of gameboard clicks
-gameboardDiv.addEventListener('click',()=>{
-    clicks += 1;
-    let clicksRemaining = maxClicks - clicks;
-    counterDiv.textContent = clicksRemaining;
-    if(clicksRemaining<=0){
-        clicks = 0;
-        clicksRemaining = 0;
-        bet = 0;
-        score = score;
-        counterDiv.textContent = clicksRemaining;
-        insultDiv.textContent = 'Loser :)'
-        timer = timer;
-    }
-
-
-    //Update Multiplier after every click
-    changeMulti(clicks);
-
-    //display score after every click
-    displayScore(clicks);
-    
-    //display different insult for every click
-    displayInsult();
-})
-
-    //Create event listener on black box; save timer
-findMeDiv.addEventListener('click',()=>{
-    clearInterval(startTimer)
-    if(moneyBag!=0){
-        moneyBag += bet + score;
-    }
-    else{
-        moneyBag += score;
-    }
-    document.getElementById('money-bag').textContent = moneyBag;
-    findMeDiv.style.backgroundColor = 'red';
-<<<<<<< HEAD
-    insultDiv.innerHTML = insults[5];
-    this.removeEventListener('click', ()=>{});
-    gameboardDiv.removeEventListener('click', ()=>{});
-    
-=======
-    insultDiv.textContent = insults[5];
->>>>>>> bb022d3dbd51aed6731138c4f2f97c9a08c868e8
-})
-})
-
-// //Create event listener on black box; save timer
-// findMeDiv.addEventListener('click',()=>{
-//     timer = 0;
-//     if(moneyBag!=0){
-//         moneyBag += bet + score;
-//     }
-//     else{
-//         moneyBag += score;
-//     }
-//     document.getElementById('money-bag').textContent = moneyBag;
-//     findMeDiv.style.backgroundColor = 'red';
-//     insultDiv.innerHTML = insults[5];
-// })
-
-//Display score Function
-function displayScore(clicks){
-    score = (initScore - clicks)*multiplier;
-    scoreDiv.textContent = score;
-    console.log(clicks, score);
+    console.log('changed', presetDiff.value)
 }
 
-//Clicks to change multiplier. Will need to cycle back to zero once max multi is reached.
 
-function changeMulti(clicks){
-    if (clicks%10===0){
-        multiplier++;
-        if (multiplier>5){
-            multiplier=5;
-        };
-        multiplerDiv.textContent = multiplier;
+function setBoxSize(size){
+    boxSize = 'width:' + size +'height:'+ size + ';';
+    diffState = boxSize;
+    findMe.style.cssText = boxSize;
+    exampleDot.style.cssText = boxSize;
+}
+
+
+function look4Clicks(){
+        maxClicks--;
+        counter.textContent = maxClicks;
+        clicks++;
+        scoreboard.textContent = clicks;
+        displayScore()
     }
-}
 
-//Change difficulty function
-
-function setDifficulty(size){
-    findMeDiv.style.width = size;
-    findMeDiv.style.height = size;
-    boxsize = 'width:' + size+'; height:'+size+';';
-    return boxsize
-}
-
-//timer
 
 function myTimer(){    
     timer--;
@@ -225,14 +200,29 @@ function myTimer(){
     if(timer <=0){
         timerDiv.textContent = 0;
         timer = 0;
-    }
+        displayScore();
+        }
     };
 
-//insult
 
-function displayInsult(){
-    let i = Math.floor(Math.random()*insults.length);
-    insultDiv.textContent = insults[i];
-    console.log(insults[i])
+function displayScore(){
+    if(timer!=0 && maxClicks!=0){
+         score = maxClicks + Number(betInput.value) + Number(timer);
+        scoreboard.textContent = score; 
+        }
+    else{
+            scoreboard.textContent = 0;
+            gameboard.removeEventListener('click',look4Clicks);
+        }
+        
+    }
 
-}
+
+    function moveBox(){
+        let topDist = Math.floor(Math.random()* (gameboard.offsetHeight - findMe.offsetHeight));
+        let leftDist = Math.floor(Math.random()* (gameboard.offsetWidth- findMe.offsetWidth));
+        findMe.style.top = topDist + 'px;';
+        findMe.style.top = leftDist + 'px;';
+        boxLocation = 'top:' + topDist + 'px; left:' + leftDist + 'px;' + diffState + ';';
+        return boxLocation
+    }
